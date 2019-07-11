f224ba7617b903bc56287372837f0cfe1779ce38
1992606 <1992606@E20735.CEGEPGIM.LCL>
2019-07-11 14:36:39 -0400

    Second commit in Netbeans

--- src/madzoonetbeans/MADZooNetbeans.java
+++ src/madzoonetbeans/MADZooNetbeans.java
@@ -17,7 +17,7 @@ public class MADZooNetbeans {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("MADZoo created in Netbeans");
-                
+        System.out.println("Welcome.....");
     }
     
 }
